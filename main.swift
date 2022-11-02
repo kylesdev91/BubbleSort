f71d0c695d1b17e07f7e640ff641a9c1523fcbd9
Kyle Hernandez <kyle.hernandez@student.allenisd.org>
2022-11-01 20:01:51 -0500

    added prepration

--- main.swift
+++ main.swift
@@ -1,14 +1,8 @@
+var newArray: [String] = []
+while let nextLine = readLine() {
+    newArray.append(nextLine)
+}
 
-
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-
-// Add your code below:
-var newArray = unsortedIntegers
 var totalSwaps = 0
 var passes = 0
 var swap = 0
@@ -16,7 +10,7 @@ print("Pass: \(passes), Swaps: \(swap)/\(totalSwaps), Array: \(newArray)")
 
 while true {
     for i in 1..<newArray.count {
-        if(newArray[i-1] > newArray[i]) {
+        if(newArray[i-1].lowercased() > newArray[i].lowercased()) {
             swap += 1
             totalSwaps += 1
             swap(integers:&newArray,firstIndex:i,secondIndex:i-1)
@@ -30,7 +24,7 @@ while true {
     }
     swap = 0
 }
-func swap(integers: inout[Int], firstIndex:Int, secondIndex: Int)  {
+func swap(integers: inout[String], firstIndex:Int, secondIndex: Int)  {
     let holder = integers[firstIndex]
     integers[firstIndex] = integers[secondIndex]
     integers[secondIndex] = holder
